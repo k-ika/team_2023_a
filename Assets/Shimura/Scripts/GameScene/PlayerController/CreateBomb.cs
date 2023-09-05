706f615c0463ffae7a6af8bbedf24de3f5953ab3
SiMSiMCard <s2310116.shimura.cgp@gmail.com>
2023-09-05 23:55:21 +0900

    爆弾を投げれるようにした
    左クリックで爆弾を投げれるようにした
    内容は分かってないです

--- Assets/Shimura/Scripts/GameScene/PlayerController/CreateBomb.cs
+++ Assets/Shimura/Scripts/GameScene/PlayerController/CreateBomb.cs
@@ -4,17 +4,21 @@ using UnityEngine;
 
 public class CreateBomb : MonoBehaviour
 {
+    public GameObject ball;
+    Rigidbody rb_ball;
+    public float thrust = 100f;
+ 
     void Start()
     {
         
     }
-
+ 
     void Update()
     {
-        //左クリックされたとき
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // マウスの左クリックをしたとき
         {
-
+            rb_ball = Instantiate(ball, transform.position, transform.rotation).GetComponent<Rigidbody>(); // 玉を生成
+            rb_ball.AddForce(transform.forward * thrust, ForceMode.Impulse); // カーソルの方向に力を一度加える
         }
     }
 }
